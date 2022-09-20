9c388cc0ed9ffcaed739238a7633f02106352d93
Stanislav <Arcanalord007@gmail.com>
2022-09-20 09:17:19 +0300

    Make core

--- js.js
+++ js.js
@@ -1,95 +1,72 @@
-let playerChoice = document.querySelector('form');
-let output = document.querySelector('.output');
-let counter = document.querySelector('.counter');
-let inputRock = document.querySelector('input.rock');
-let inputPaper = document.querySelector('input.paper');
-let inputScissors = document.querySelector('input.scissors');
-let finalDisplay = document.querySelector('#win-lose-state');
-let winLoseMessage = document.querySelector('.win-lose-message');
-let restartButton = document.querySelector('#restart-button');
-let winCounter = 0;
-let loseCounter = 0;
-let getPlayerChoice = playerChoice.addEventListener('click', e => playRound(e.target.className));
+const states = ['rock,scissors,win', 'paper,rock,win', 'scissors,paper,win',
+     'scissors,rock,lose', 'rock,paper,lose', 'paper,scissors,lose'];
 
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3 + 1);
-    switch(computerChoice) {
-        case 1:
+const userInput = document.querySelectorAll('.button');
+const resultPlacement = document.querySelector('.output');
+const scorePlayer = document.querySelector('.score-player')
+const scorePc = document.querySelector('.score-pc')
+let roundsCounter = 0;
+let countWinPlayer = 0;
+let countWinPc = 0;
+
+
+
+let userChoice = userInput.forEach(e => e.addEventListener('click', getUserValue));
+
+function getUserValue() {
+    let userChoice = this.id
+    playRound(userChoice, getComputerValue());
+}
+
+function getComputerValue() {
+let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
             return 'rock';
-        case 2:
+        case 1:
             return 'paper';
-        case 3:
+        case 2: 
             return 'scissors';
-        default:
-            console.log('Problem');
     }
 }
 
-function playRound(playerChoice) {
-    console.log(playerChoice);
-    let computerChoice = getComputerChoice()
-    if (computerChoice === playerChoice) {
-        messageDisplayed('Draw:c');
-        return;
-    } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
-        messageDisplayed('You Lose! Rock beats Scissors');
-    } else if (computerChoice === 'paper' && playerChoice === 'rock') {
-        messageDisplayed('You Lose! Paper beats Rock');
-    } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
-        messageDisplayed('You Lose! Scissors beats Paper');
+function playRound(user, comp) {
+    roundsCounter++;
+    let input = `${user},${comp}`;
+    if (user === comp) {
+        // console.log('draw')
     } else {
-        messageDisplayed('Win!');
-        winCounter++;
-        counterWinLose();
-        return;
+        let compare = states.filter(value => value.startsWith(input));
+        let result = compare[0].split(',')
+        scoreIteration(result[2]);
     }
-    loseCounter++;
-    counterWinLose();
+    checkRoundsPlayed();
+}
 
+function checkRoundsPlayed() {
 
 }
 
-function messageDisplayed(text) {
-    output.innerHTML = '';
-    let message = document.createElement('p');
-    output.appendChild(message).textContent = text;
+function screenInfoUpdate() {
+    resultPlacement.textContent = 'hi';
+    console.log('')
 }
 
-function counterWinLose() {
-    counter.textContent = `Win: ${winCounter} Lose: ${loseCounter}`;
-    if (winCounter === 5) {
-        outOfFiveResult(true);
-    } else if (loseCounter === 5) {
-        outOfFiveResult(false);
+function scoreIteration(result) {
+    if (result === 'win') {
+        countWinPlayer++;
+        scorePlayer.textContent = countWinPlayer;
+    } else if (result === 'lose') {
+        countWinPc++;
+        scorePc.textContent = countWinPc;
     }
 }
+scoreIteration();
+function createScore() {
 
-function outOfFiveResult(result) {
-    messageDisplayed('');
-    counter.textContent = '';
-    inputRock.disabled = true;
-    inputPaper.disabled = true;
-    inputScissors.disabled = true;
-    restartButton.classList.add('visible');
-    finalDisplay.classList.add('winning-message');
-    if (result) {
-        winLoseMessage.textContent = `Congratulation! You Win!`;
-    } else {
-        winLoseMessage.textContent = `ILLUMINATED`;
-    }
-    winCounter = 0;
-    loseCounter = 0;
-}    
-
-restartButton.addEventListener('click', restart);
-
-function restart() {
-    restartButton.classList.remove('visible');
-    finalDisplay.classList.remove('winning-message');
-    inputRock.disabled = false;
-    inputPaper.disabled = false;
-    inputScissors.disabled = false;
-    winLoseMessage.textContent = '';
 }
 
-//to the center of the screen with a button to start again - v
+function updateScore() {
+
+}
+screenInfoUpdate();
